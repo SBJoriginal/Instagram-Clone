6748288d96f8ebdce6c13b9f5ca591b64bb0afa5
sofiane-hamdini <sofiane.hamdini.1@ulaval.ca>
2026-01-25 20:26:53 -0500

    feat : Added connection between homepage and landing page

--- frontend/src/app/pages/shell/navigation/navigation.component.ts
+++ frontend/src/app/pages/shell/navigation/navigation.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { MatIcon } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LogoComponent } from '../../../shared/ui/logo/logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [MatIcon, LogoComponent, MatButtonModule],
+  imports: [MatIconModule, LogoComponent, MatButtonModule, RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
-export class NavigationComponent {
-
-}
+export class NavigationComponent {}
