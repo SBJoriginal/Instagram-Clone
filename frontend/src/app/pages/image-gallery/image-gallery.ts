9cf62995470909989e154860a2f3728677220b31
stevkouakam <144498156+stevkouakam@users.noreply.github.com>
2026-02-02 12:35:16 -0500

    refactor: use mat-card in image-gallery to match team style

--- frontend/src/app/pages/image-gallery/image-gallery.ts
+++ frontend/src/app/pages/image-gallery/image-gallery.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { ImagePost } from '../../models/image.model';
 
 @Component({
   selector: 'app-image-gallery',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './image-gallery.html',
   styleUrl: './image-gallery.css',
 })
