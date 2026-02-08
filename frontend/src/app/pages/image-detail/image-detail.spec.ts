c77cc8903997e6397ad9bf1a474bdd9101cb21c9
Lenny Plante <lsbjoriginal@gmail.com>
2026-02-07 19:02:16 -0500

    Update image-detail.spec.ts

--- frontend/src/app/pages/image-detail/image-detail.spec.ts
+++ frontend/src/app/pages/image-detail/image-detail.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ImageDetail } from './image-detail';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ImageDetail', () => {
   let component: ImageDetail;
@@ -9,6 +11,11 @@ describe('ImageDetail', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ImageDetail],
+      providers: [
+        provideRouter([]), 
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ImageDetail);
