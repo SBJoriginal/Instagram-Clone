2bfc3999238209967acc27771c97513b5c1052d3
stevkouakam <144498156+stevkouakam@users.noreply.github.com>
2026-03-04 01:37:02 -0500

    style: apply prettier formatting

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8081/api',
-  googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER',
+  googleClientId: '999444988899-bjno73ojihr6c5b18jbkeugrahrvd1di.apps.googleusercontent.com',
 };
