3eaa4ac5785f920fe3f5c5aaa25fdfd7ac23fd23
Lenny Plante <lsbjoriginal@gmail.com>
2026-03-10 12:59:53 -0400

    edit profile/sign up fix

--- frontend/src/app/pages/complete-profile/complete-profile.component.ts
+++ frontend/src/app/pages/complete-profile/complete-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
@@ -11,6 +11,7 @@ import { ProfileService } from '../../services/profile.service';
 import { TokenService } from '../../services/token.service';
 import { BackArrowComponent } from '../../shared/ui/back-arrow/back-arrow.component';
 import { LogoComponent } from '../../shared/ui/logo/logo.component';
+import { ProfileValidators } from '../../validators/profile.validators';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
@@ -49,7 +50,6 @@ export class CompleteProfileComponent implements OnInit {
       },
     });
 
-    // Handle username availability check
     this.profileForm
       .get('username')
       ?.valueChanges.pipe(debounceTime(300), distinctUntilChanged())
@@ -62,10 +62,10 @@ export class CompleteProfileComponent implements OnInit {
   }
 
   protected readonly profileForm = this.fb.group({
-    username: ['', [Validators.required]],
-    firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\s'-]+$/)]],
-    lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\s'-]+$/)]],
-    phoneNumber: ['', [Validators.required, Validators.pattern(/^\d{3}-\d{3}-\d{4}$/)]],
+    username: ['', ProfileValidators.username],
+    firstName: ['', ProfileValidators.name],
+    lastName: ['', ProfileValidators.name],
+    phoneNumber: ['', ProfileValidators.phone],
   });
 
   protected onPhoneInput(event: Event): void {
