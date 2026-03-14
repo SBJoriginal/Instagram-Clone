58ace9a21cc30e79733d158af5125eab9228d605
jimmy roux <127576897+mijobmij@users.noreply.github.com>
2026-03-14 10:27:49 -0400

    fix(env):externalise sentry key from environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,5 @@ export const environment: Environment = {
   production: true,
   apiUrl: 'http://localhost:8081/api',
   googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER',
-  sentryDsn:
-    'https://de18f217e92ee8cbd7c8fb07293763d0@o4511023128182784.ingest.us.sentry.io/4511023134277632',
+  sentryDsn: 'SENTRY_DSN_PLACEHOLDER',
 };
