781c9553deeb77e5e1a0061ef46a6dc7eec87341
stevkouakam <144498156+stevkouakam@users.noreply.github.com>
2026-03-31 14:08:05 -0400

    fix : correction

--- frontend/src/app/services/analytics.service.ts
+++ frontend/src/app/services/analytics.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-declare let gtag: Function;
+declare let gtag: (...args: unknown[]) => void;
 
 @Injectable({ providedIn: 'root' })
 export class AnalyticsService {
